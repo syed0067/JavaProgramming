9649b1e4a2a4ab5176b3ef769db96cf9b13820e0
syed0067 <syedghouse0067@gmail.com>
2021-10-09 20:57:57 +0530

    My secondcommit

--- src/com/starpattern/Main.java
+++ src/com/starpattern/Main.java
@@ -3,5 +3,6 @@ package com.starpattern;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("My name is syed");
     }
 }
